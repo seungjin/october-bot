4f45b2776b8acfeec495395132e66e37cc6caebb
seungjin <seungjin@email.arizona.edu>
2010-10-04 09:23:20 -0700

    first commit

---
+++ OctoberbotApp.java
@@ -0,0 +1,44 @@
+/*
+ * OctoberbotApp.java
+ */
+
+package octoberbot;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class OctoberbotApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new OctoberbotView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of OctoberbotApp
+     */
+    public static OctoberbotApp getApplication() {
+        return Application.getInstance(OctoberbotApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(OctoberbotApp.class, args);
+    }
+}
